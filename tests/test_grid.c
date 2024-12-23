64ce4eadf4ef63655d9ca6f7302437e952e1d41f
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-23 11:48:15 +0100

    Update test_grid.c
    correction on the test file , wrong route path , wrong collision code and change on grid.h

--- tests/test_grid.c
+++ tests/test_grid.c
@@ -27,13 +27,13 @@ int main(void)
     grid = initGrid(10, 10);
     collision = 0;
     for (i = 0; i < 4; i++) {
-        collision = moveRider(grid, grid->player, grid->playerRoute);
+        collision = moveRider(grid, grid->player, &grid->playerRoute);
         assert(collision == 0);
         assert(grid->player->direction == UP);
         assert(grid->player->pos_x == 6);
         assert(grid->player->pos_y == 5 - (i + 1));
     }
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -44,11 +44,11 @@ int main(void)
     grid->player->pos_x = 6;
     grid->player->pos_y = 6;
     for (i = 0; i < 3; i++) {
-        collision = moveRider(grid, grid->player, grid->playerRoute);
+        collision = moveRider(grid, grid->player, &grid->playerRoute);
         assert(collision == 0);
         assert(grid->player->pos_y == 6 + (i + 1));
     }
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -59,11 +59,11 @@ int main(void)
     grid->player->pos_x = 6;
     grid->player->pos_y = 6;
     for (i = 0; i < 3; i++) {
-        collision = moveRider(grid, grid->player, grid->playerRoute);
+        collision = moveRider(grid, grid->player, &grid->playerRoute);
         assert(collision == 0);
         assert(grid->player->pos_x == 6 + (i + 1));
     }
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -72,13 +72,13 @@ int main(void)
     collision = 0;
     grid->player->direction = LEFT;
     grid->player->pos_x = 4;
-    grid->player->pos_y = 5;
+    grid->player->pos_y = 4;
     for (i = 0; i < 3; i++) {
-        collision = moveRider(grid, grid->player, grid->playerRoute);
+        collision = moveRider(grid, grid->player, &grid->playerRoute);
         assert(collision == 0);
         assert(grid->player->pos_x == 4 - (i + 1));
     }
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -88,30 +88,30 @@ int main(void)
     grid->player->pos_x = 6;
     grid->player->pos_y = 6;
     grid->player->direction = UP;
-    moveRider(grid, grid->player, grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
 
     grid->player->direction = RIGHT;
-    moveRider(grid, grid->player, grid->playerRoute);
-    moveRider(grid, grid->player, grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
 
     grid->player->direction = DOWN;
-    moveRider(grid, grid->player, grid->playerRoute);
-    moveRider(grid, grid->player, grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
 
     grid->player->direction = LEFT;
-    moveRider(grid, grid->player, grid->playerRoute);
-    moveRider(grid, grid->player, grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
 
-    sx[0] = 6;  sy[0] = 5;
-    sx[1] = 7;  sy[1] = 5;
-    sx[2] = 7;  sy[2] = 5;
-    sx[3] = 7;  sy[3] = 6;
-    sx[4] = 7;  sy[4] = 6;
-    sx[5] = 6;  sy[5] = 6;
+    sx[0] = 7;  sy[0] = 7;
+    sx[1] = 8;  sy[1] = 7;
+    sx[2] = 8;  sy[2] = 6;
+    sx[3] = 8;  sy[3] = 5;
+    sx[4] = 7;  sy[4] = 5;
+    sx[5] = 6;  sy[5] = 5;
     sx[6] = 6;  sy[6] = 6;
 
     point = grid->playerRoute;
-    for (i = 6; i >= 0; i--) {
+    for (i = 0; i !=7; i++) {
         assert(point != NULL);
         assert(sx[i] == point->pos_x);
         assert(sy[i] == point->pos_y);
@@ -124,10 +124,10 @@ int main(void)
     grid->player->pos_x = 5;
     grid->player->pos_y = 5;
     grid->player->direction = UP;
-    moveRider(grid, grid->player, grid->playerRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
 
     grid->player->direction = DOWN;
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -136,13 +136,13 @@ int main(void)
     grid->bot->pos_x = 3;
     grid->bot->pos_y = 3;
     grid->bot->direction = RIGHT;
-    moveRider(grid, grid->bot, grid->botRoute);
-    moveRider(grid, grid->bot, grid->botRoute);
+    moveRider(grid, grid->bot, &grid->botRoute);
+    moveRider(grid, grid->bot, &grid->botRoute);
 
     grid->player->pos_x = 4;
     grid->player->pos_y = 4;
     grid->player->direction = UP;
-    collision = moveRider(grid, grid->player, grid->playerRoute);
+    collision = moveRider(grid, grid->player, &grid->playerRoute);
     assert(collision == 2);
     freeGrid(grid);
 
@@ -156,8 +156,8 @@ int main(void)
     grid->bot->pos_y = 5;
     grid->bot->direction = RIGHT;
 
-    moveRider(grid, grid->player, grid->playerRoute);
-    collision = moveRider(grid, grid->bot, grid->botRoute);
+    moveRider(grid, grid->player, &grid->playerRoute);
+    collision = moveRider(grid, grid->bot, &grid->botRoute);
     assert(collision == 3);
     freeGrid(grid);
 
