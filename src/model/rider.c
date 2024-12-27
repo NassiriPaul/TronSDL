ecf0d68f2f2a01c6e18233b5d5950ec3d516d1f2
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-27 16:53:53 +0100

    Update rider.c
    start each round with 3 turbos

--- src/model/rider.c
+++ src/model/rider.c
@@ -6,7 +6,7 @@ Rider* initRider (int x, int y) {
 	Rider* rider = (Rider*) malloc (sizeof(Rider));
 	rider->pos_x = x;
 	rider->pos_y = y;
-	rider->turbos = 0;
+	rider->turbos = 3;
 	rider->direction = UP;
 	return rider;
 }
