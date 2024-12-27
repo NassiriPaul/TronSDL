0c84cf99b7def90409c309625b5acbb08d1c885f
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-27 18:02:11 +0100

    Update view.h

--- include/view.h
+++ include/view.h
@@ -4,5 +4,6 @@
 void viewInit();
 void viewStart(Grid *grid, int scorePlayer, int scoreBot);
 void viewUpdate(Grid *grid);
+void updateViewTurbos(int n_lines, int turbos);
 void updateViewScore(int n_lines, int scorePlayer, int scoreBot);
 void viewCleanup();
