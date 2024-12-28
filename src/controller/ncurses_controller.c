44bf2f9e1aa321cca2b5e2687787b3db220c9a50
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-28 23:20:57 +0100

    Update ncurses_controller.c
    add doxygen comments

--- src/controller/ncurses_controller.c
+++ src/controller/ncurses_controller.c
@@ -1,45 +1,23 @@
-#include <time.h>       /* clock_gettime, struct timespec, etc. (POSIX) */
-#include <ncurses.h>    /* noecho(), halfdelay(), getch(), etc.         */
-#include "types.h"      /* For DIRECTIONS enum, etc.                    */
-#include <sys/time.h>   /* gettimeofday, struct timeval                 */
+#include <time.h>
+#include <ncurses.h>
+#include "types.h"
+#include <sys/time.h> /* gettimeofday, struct timeval*/
 
-int getInput(int* direction, unsigned long ms)
+int getInput(int* input, unsigned long ms)
 {
-    struct timeval start, stop;
-    unsigned long elapsed;     /* elapsed time in milliseconds */
+    struct timeval start, stop; /* timestamps to get time elapsed and time remaining */
+    unsigned long elapsed; /* elapsed time in milliseconds */
     unsigned long remaining;
     int ch;
 
     gettimeofday(&start, NULL);
 
-    /*
-     * halfdelay() expects tenths of a second. 
-     * For example, if ms=1000, ms/100=10 -> halfdelay(10) = 1 second max.
-     */
-    halfdelay((int)(ms / 100));
+    halfdelay((int)(ms / 100));/* say to ncurses to wait ms milliseconds to get an input. halfdelay take tenth of seconds that's why it is divide by 100 */
     noecho();
+    
+    ch = getch();
 
-    /* Attempt to read one character within halfdelay(...) */
-    ch = getch();  /* returns ERR if no key pressed within that time */
-
-    /* Now record the end time */
-    gettimeofday(&stop, NULL);
-
-    /* Convert difference to milliseconds:
-       (stop.tv_sec - start.tv_sec) -> seconds difference
-       (stop.tv_usec - start.tv_usec) -> microseconds difference
-    */
-    elapsed = (unsigned long)((stop.tv_sec - start.tv_sec) * 1000UL)
-            + (unsigned long)((stop.tv_usec - start.tv_usec) / 1000UL);
-
-    /* If user took more than ms, clamp to ms */
-    if (elapsed > ms) {
-        elapsed = ms;
-    }
-
-    remaining = ms - elapsed;
-
-    /* Interpret the key */
+    * Interpret the input */
     switch (ch) {
         case KEY_UP:    
             *direction = UP;    
@@ -55,9 +33,21 @@ int getInput(int* direction, unsigned long ms)
             break;
         case 116:  /*Touche t*/
             *direction = 5;  
-            break;
-            
+            break;  
+    }
+    
+    /* Now record the end time */
+    gettimeofday(&stop, NULL);
+
+    /* Convert difference to milliseconds */
+    elapsed = (unsigned long)((stop.tv_sec - start.tv_sec) * 1000UL) + (unsigned long)((stop.tv_usec - start.tv_usec) / 1000UL);
+
+    /*  constrain the answer */
+    if (elapsed > ms) {
+        elapsed = ms;
     }
 
-    return (int)remaining;  /* Return leftover time in ms */
+    remaining = ms - elapsed;
+
+    return (int)remaining;
 }
