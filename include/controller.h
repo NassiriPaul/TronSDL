dbbdad8f92be71702a15b065367b8cc5a76aece9
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-28 21:39:51 +0100

    Update controller.h
    change doxygen comments

--- include/controller.h
+++ include/controller.h
@@ -6,15 +6,18 @@
  * 
  * @param n_lines number of lines in the grid
  * @param n_columns number of colunms in the grid
+ *
  * @return the winner. 1 for the player and 0 for the bot
  */
 int startGame(int n_lines, int n_columns);
 
 /**
- * @brief Get the player's direction within a specified timeout (in tenths of a second).
- * 
- * @param direction Pointer to the current direction of the player.
- * @param timeout Timeout in tenths of a second to capture input.
- * @return Remaining time in tenths of a second after user input or timeout.
+ * @brief Get the player's input within a specified timeout (in ms).
+ *
+ * inputs are casted as follow : 0 top, 1 right, 2 down, 3 left, 5 turbos and 6 quit
+ * @param input A pointer to stock the input of the player ( turbo, quit or movement )
+ * @param ms The player have ms milliseconds to send an input; else nothing new is stored in ms
+ *
+ * @return if the user sent an input before the timeout, we return the time he had left in ms
  */
 int getInput(int* input, unsigned long ms);
