637e3a11f4b557977425a5456d7a03d88473d96d
Nassiri Paul <57369097+NassiriPaul@users.noreply.github.com>
2024-12-28 21:47:44 +0100

    Update types.h
    remove unused stuff and add doxygen comments

--- include/types.h
+++ include/types.h
@@ -1,10 +1,12 @@
 #pragma once
-typedef enum {UP, RIGHT, DOWN, LEFT} DIRECTIONS;
-typedef enum {BOT, PLAYER} RIDERS;
 
-typedef struct{
-    int x;
-    int y;
-} Pos;
-
-Pos* addPos (int x, int y); /*pas sur de son utiliter (si on malloc rider par exemple, on malloc aussi position en meme temps)*/
+/**
+ * @enum DIRECTIONS
+ * @brief Represent the possible movement directions.
+ */
+typedef enum {
+    UP, 
+    RIGHT,
+    DOWN,
+    LEFT
+} DIRECTIONS;
